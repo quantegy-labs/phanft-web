2a2353cd38c0bdf69a7dd43ff9ba2a4ed4983376
Drew Cook <drewcook29@gmail.com>
2022-05-23 20:27:34 -0600

    chore: Port over existing content

--- pages/faq.tsx
+++ pages/faq.tsx
@@ -10,7 +10,255 @@ const FaqPage: NextPage = () => {
 				<title>PhanFT | Frequently Asked Questions</title>
 			</Head>
 			<Layout>
-				<h1>Frequently Asked Questions</h1>
+				<h1>NFTs for Noobs</h1>
+				<p>Just relax you&apos;re doing fine</p>
+				<p>
+					We appreciate that NFTs, blockchain, and cryptocurrency are all new territories – each
+					with their own steep learning curve and risks to be aware of. We also know that for many
+					of you, this will be your first time learning about and/or investing in an NFT project.
+				</p>
+				<p>
+					In this new and fast evolving space we are all n00bs and it’s best to approach it all with
+					an open, learner’s mind.
+				</p>
+				<p>
+					We’ve put together this Helping Phriendly Guide (FAQ) to help you navigate the important
+					things to understand.
+				</p>
+				<p>
+					Please jump to the glossary section at the bottom for simple explanations of the key terms
+					and concepts here.
+				</p>
+				<hr />
+				<h2>FAQs</h2>
+				<div>What is Discord?</div>
+				<div>
+					Discord hosts our PhanFT community hub. It's a place to get real time updates on the
+					project, hang out, engage with the community, discuss a variety of interests, and plants
+					seeds for serendipity. It has become the gold standard community platform for NFT
+					projects, and by the end of 2022 we expect every brand will have one.{' '}
+					<a href="https://discord.com/invite/P5VpF5f7qx" target="_blank" rel="noreferrer">
+						Join here
+					</a>
+					.
+				</div>
+				<div>What is an NFT?</div>
+				<div>
+					NFT stands for “Non-Fungible Token” and it is a unique, one-of-a-kind digital item that
+					users can buy, own, and trade on the blockchain. NFTs are bought and sold using
+					cryptocurrencies (most commonly Ethererum).
+				</div>
+				<div>Where on the Blockchain do NFTs live?</div>
+				<div>
+					Currently, most NFTs on the market live on the Ethereum blockchain. But there are other
+					blockchains as well supporting NFTs including Solana, Polygon, and others.
+				</div>
+				<div>Okay, but what can an NFT be?</div>
+				<div>
+					An NFT can represent almost anything such as: Digital Collectibles Artwork Concert Tickets
+					Video Games Music &amp; Publishing Contracts to real-world assets such as Real Estate
+					Digital ID and Avatar Club Membership Cards And many more…
+				</div>
+				<div>How is ownership of the NFT guaranteed?</div>
+				<div>
+					Ownership is written into the smart contract that lives on the blockchain forever and
+					governs the rights of that digital asset. This is what allows digital items such as images
+					(which as we know them historically are easily cloneable) to now be bought, sold, or
+					traded with both trust and transparency.
+				</div>
+				<div>What are the essential characteristics and benefits of NFTs?</div>
+				<div>
+					Unique – An NFT&rsquo;s metadata describes what makes this asset different from all the
+					rest in a collection. This is a permanent, unalterable record that describes what this NFT
+					represents, like the certificate of authenticity that you&rsquo;d get with a rare
+					painting. Rare – Scarcity is an important ingredient in the recipe that makes NFTs so
+					attractive. While developers have the freedom to generate an infinite supply of certain
+					assets, they also have the power to limit the number of rare, desirable items in
+					existence. This preserves value. It&rsquo;s the same reason a physical show print is made
+					in limited allotments. Sure, the artist could print 20,000 for the whole venue but they
+					wouldn&rsquo;t sell them all, and they wouldn&rsquo;t hold long-term value. Supply and
+					demand economics holds true with NFTs too. Ownership – Blockchain technology proves your
+					ownership while making the change of ownership rights easy. Just like you own a physical
+					piece of art (like a show poster), you own this digital art. You can choose to keep it,
+					sell it, trade it, commercialize it, or exploit it in other ways as you see fit, depending
+					on the rights assigned to you in the smart contract. Transferability – NFTs can be freely
+					traded, transferred, or sold. Security &amp; Authenticity – The smart contract behind the
+					NFT prevents fraud prevention, a problem affecting everything from art to concert tickets,
+					collectibles, fashion, even wine. Blockchain powered NFTs are the perfect weapon against
+					counterfeiting while giving buyers confidence that they&rsquo;ll get what they paid for.
+				</div>
+				<div>
+					Why can’t I just right-click and save these digital items or take a screenshot of it?
+				</div>
+				<div>
+					Sure, you could take a screenshot but you don&rsquo;t own it. You can&rsquo;t sell it,
+					trade it, or prove you own it on the blockchain. You can&rsquo;t redeem it for perks and
+					accessibilities that owning the real one would grant you. For ex: I can take a picture of
+					a $100 bill but it doesn&rsquo;t mean I actually have that money, and no merchant would
+					accept my picture of that bill. I can take a picture of a gym membership card, but if I
+					tried to use it, it wouldn&rsquo;t grant me access to the gym.
+				</div>
+				<div>How do I mint an NFT?</div>
+				<div>
+					Here&rsquo;s the general process to buying an NFT from an on-sale (aka minting). Step 1:
+					you need to buy some Ethererum. It can be purchased easily on Coinbase using dollars or
+					most other global currencies. If you&rsquo;re new to buying Ethereum (or any other
+					cryptocurrency) we recommend using Coinbase. Step 2: to transfer the Ethereum you bought
+					(or already owned), to a wallet that can purchase the NFT. We recommend Metamask. You can
+					download and install as a Chrome Browser Extension, and as a mobile app for iOS and
+					Android. Here&rsquo;s an instructional{' '}
+					<a href="https://www.youtube.com/watch?v=ZIGUC9JAAw8" target="_blank" rel="noreferrer">
+						<strong>
+							<em>YouTube video</em>
+						</strong>
+					</a>{' '}
+					showing how. Step 3: Transfer the necessary amount of Ethereum (ETH) + .05 to .10 extra
+					ETH for gas fees (i.e. transactions fees) to your Metamask wallet. Step 4: Authenticate
+					your wallet on the website where you&rsquo;ll be buying the NFT by pressing the “Connect
+					Wallet” button. Step 5: When the mint is live, press the “Mint” or “Buy” button on the
+					website, and enter the number of NFTs you want to buy. Metamask will pop up and
+					you&rsquo;ll be able to see estimated gas fees and approve or reject the transaction. If
+					you&rsquo;re okay with the fees click “approve” and you&rsquo;re done! You&rsquo;ll know
+					whether the mint was successful and will soon see the NFT appear in your Metamask mobile
+					wallet. Note: you can&rsquo;t view your NFTs in the Metamask desktop Chrome extension.
+					Note: when we announce the minting date for The Lizards we&rsquo;ll be publishing a
+					separate “Rollout” section to the website to walk you through our specifics.
+				</div>
+				<div>What are gas fees? Do I need gas to buy an NFT?</div>
+				<div>
+					“Gas” is basically units of energy that&rsquo;s burned during a transaction on the
+					Ethereum network, while computers are verifying transactions and adding them to the
+					blockchain. Think of it as a transaction fee, and it goes directly to Ethereum miners who
+					provide the computer power necessary to verify transactions. Gas prices fluctuate based on
+					the demand at any given time. PhanFT does not receive a percentage of gas fees and has no
+					control over network congestion or gas prices.
+				</div>
+				<div>If I mint a lizard, how will I know what traits and attributes its art will have?</div>
+				<div>
+					Think of it like opening a baseball card pack and pulling one card. You don&rsquo;t know
+					which player you get. With our Lizard collection, each Lizard is unique and different in
+					traits, with some more rare than others. You have the option of minting up to 5 Lizards
+					per wallet address at the on-sale. If you&rsquo;re seeking to acquire a rarer one, or find
+					another in the collection whose art you like better, you can always trade with other
+					holders or sell them. Likewise, you could equally mint one of the rarest, most unique, and
+					cool looking ones of the whole collection. This includes a Rutherford The Brave, of which
+					25 are being included in the collection.
+				</div>
+				<div>What if I want to sell my lizard NFT sometime after I buy it?</div>
+				<div>
+					You can sell your Lizard(s) anytime. There are several marketplaces where you can buy/sell
+					NFTs, the market leader being OpenSea. OpenSea is like eBay for buying and selling NFTs.
+					We encourage you to check the site out and spend some time exploring.
+				</div>
+				<div>How can I make money here?</div>
+				<div>
+					When the Lizard collection is sold out, there will be a secondary market for buyers and
+					sellers. As it stands today, most of this activity will happen on OpenSea.io. Once more
+					phans want to own a lizard than ones available, you&rsquo;d be able to sell yours for a
+					profit. We recommend buying multiple Lizards so you can sell some if you wish, and keep at
+					least one to remain a member of our community and receive all the phan-centric benefits
+					and perks we offer Lizard holders. Remember that all sales (including secondary market
+					sales) pay a royalty to the artist, which will make them very happy.
+				</div>
+				<div>How do I help save the race of lizards from extinction?</div>
+				<div>
+					Buy at least one (1) Lizard during our Mint. It will remove it from their life of
+					purgatory in Prussia and bring them safely to the Phanaverse where they can live in peace,
+					harmony, and tranquility.
+				</div>
+				<div>Other questions you’d like us to add to this FAQ?</div>
+				<div>
+					Please email them to hello(at)phanft.com. We’ll keep this updated and as robust as
+					possible so phans with all levels of tech acumen can understand this and choose whether to
+					participate.
+				</div>
+				<hr />
+				<h2>Glossary</h2>
+				<p>Listed in order to maximize understanding</p>
+				<h3>NFT</h3>
+				<p>
+					NFT stands for “Non-Fungible Token” and it is a unique, one-of-a-kind digital item that
+					users can buy, own, and trade on the blockchain. NFTs are bought and sold using
+					cryptocurrencies.
+				</p>
+				<h3>Non-Fungibile</h3>
+				<p>
+					A non-fungible asset is something unique and irreplaceable. It can&rsquo;t be swapped for
+					the same good in like kind. An autographed book, a rare old coin, or The Great Went
+					Pollock Walking Fish, is non-fungible as they both can&rsquo;t be easily replaced. A
+					physical art print is non-fungible. An NFT is non-fungible.&nbsp;
+				</p>
+				<h3>Fungibile</h3>
+				<p>
+					In simple words, fungibility refers to something replaceable or exchangeable. An asset or
+					a good is fungible if you can replace or exchange all or part of it with another
+					indistinguishable equivalent. Currencies (imagine a $5 bill), Gold, Wheat, Oil, are all
+					fungible goods. A $5 bill is completely exchangeable with any $5 bill – its value is
+					exactly the same.
+				</p>
+				<h3>Crypto Wallets</h3>
+				<p>
+					Buying, storing, and selling NFTs is quite simple and you do it all through a crypto
+					wallet. Think of it like a digital version of the wallet you carry around in your pocket
+					or purse. Except only you hold the special keys and the password to open it. And
+					it&rsquo;s important you safeguard those keys by keeping them written somewhere safe. They
+					are the only way to access or restore your wallet. More on wallets here in this{' '}
+					<a
+						href="https://www.youtube.com/watch?v=d8IBpfs9bf4"
+						className="inner-link text-decoration-underline"
+						target="_blank"
+						rel="noreferrer"
+					>
+						<strong>explainer video on YouTube.</strong>
+					</a>
+				</p>
+				<h3>Mint or Minting</h3>
+				<p>
+					The process by which an NFT becomes part of the blockchain. Once an asset is put on the
+					blockchain, it is “minted” as a token and cannot be altered. Just like how physical coins
+					are minted and put into circulation, it&rsquo;s the same for NFTs. If you buy a Lizard
+					from us directly when they go on-sale, you are Minting a Lizard.
+				</p>
+				<h3>Ethereum</h3>
+				<p>
+					The largest blockchain with smart contract functionality. Ethereum is currently the main
+					platform for NFT projects. The Ethereum blockchain acts as a decentralized computer in
+					addition to being a distributed ledger. It can run blockchain-based applications called
+					“Smart Contracts”.
+				</p>
+				<h3>Ether (or ETH)</h3>
+				<p>
+					The cryptocurrency of the Ethereum blockchain. Ether can be purchased using fiat currency
+					such as U.S. dollars on a variety exchanges such as Coinbase, or within certain crypto
+					Wallets such as Metamask.
+				</p>
+				<h3>Smart Contacts</h3>
+				<p>
+					These are simply programs stored on a blockchain that run when predetermined conditions
+					are met. They typically are used to automate the execution of an agreement so that all
+					participants can be immediately certain of the outcome, without any intermediary&apos;s
+					involvement or time loss. In the case of PhanFT, smart contracts are how ownership is
+					determined, and how payments to multiple parties including the seller, the artist, and the
+					charity partner are automatically made when a transaction takes place.
+				</p>
+				<h3>Gas</h3>
+				<p>
+					“Gas” is used to describe a unit of measurement for the amount of computational power
+					needed for executing specific operations on the Ethereum network. When buying an NFT on
+					the Ethereum network, it&rsquo;s a cost necessary to perform the transaction. The price of
+					gas fluctuates based on network congestion at that moment. ETH Gas prices are denominated
+					in a unit known as gwei, which is a term used to refer to an amount of ETH equal to
+					0.000000001 ETH. The total gas used to mint your NFT will become your transaction fee.
+					There&rsquo;s lots of articles and YouTube videos explaining this in more depth.{' '}
+					<a href="https://youtu.be/hQ78FVSv-vs" className="inner-link">
+						Here&rsquo;s a 3 minute explainer video on YouTube.
+					</a>
+				</p>
+				<h3>NFT Marketplaces</h3>
+				<p>
+					Websites such as Opensea.io where you can buy and sell NFT items. These marketplaces are
+					similar to eBay. You can easily buy and sell NFTs here for a fixed price or in an auction.
+				</p>
 			</Layout>
 		</>
 	)
--- pages/mission.tsx
+++ pages/mission.tsx
@@ -11,6 +11,38 @@ const MissionPage: NextPage = () => {
 			</Head>
 			<Layout>
 				<h1>Why PhanFT?</h1>
+				<p>
+					The way people interact and engage digitally is evolving faster than ever. We’re at the
+					forefront of a new era where art and artists can thrive in the digital world with complete
+					transparency, authenticity, and preserved value.
+				</p>
+				<p>
+					Similar to how we phans were among the first in the mid-90’s web1.0 era to congregate and
+					build digital community in newsgroups, chatrooms, and message boards to trade tapes, share
+					show reviews and speculate over rumors; today we have a massive opportunity to become
+					digital pioneers once again, and define what a passionate music community can do within
+					the metaverse when co-creation is the core ethos.
+				</p>
+				<p>
+					We believe that blockchain technology (in particular NFTs) is the ideal vehicle to enable
+					being an active, participatory fan within a mutually beneficial ecosystem for artists and
+					collectors alike.
+				</p>
+				<p>
+					A key feature of each PhanFT™ collection is unlocking new revenue streams for creators and
+					artists, by ensuring they get a percentage of every sale. Not just the initial sale. This
+					was never possible before NFTs.
+				</p>
+				<p>
+					At PhanFT, each NFT artist will receive a royalty every time their work is bought and then
+					re-sold on the blockchain. Now, instead of making a one-time sale on an art print, the
+					artist will get a cut every time their work exchanges hands, for years and years to come.
+				</p>
+				<p>
+					Welcome to a new digital paradigm with endless possibilities. Consider this your
+					invitation to jump in the pond (with us here to guide you) as we create the greatest Phan
+					community in the metaverse.
+				</p>
 			</Layout>
 		</>
 	)
