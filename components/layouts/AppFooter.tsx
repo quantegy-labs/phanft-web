ba3d9d9809667e1bfabd38dcae337800bcb4846f
Drew Cook <drewcook29@gmail.com>
2022-06-15 17:16:24 -0600

    chore: Social icons

--- components/layouts/AppFooter.tsx
+++ components/layouts/AppFooter.tsx
@@ -1,19 +1,54 @@
 import { Box, Link, Typography } from "@mui/material";
+import Image from "next/image";
 
 const styles = {
   footer: {
     display: "flex",
-    flex: 1,
-    p: "2rem 0",
+    flexDirection: "column",
+    py: 4,
     borderTop: "1px solid #eaeaea",
     justifyContent: "center",
     alignItems: "center",
+  },
+  socialWrap: {
+    mb: 1,
   },
 };
 
 const AppFooter = (): JSX.Element => (
   <Box component="footer" sx={styles.footer}>
-    <Typography variant="overline">
+    <Box sx={styles.socialWrap}>
+      <a
+        href="https://discord.com/invite/P5VpF5f7qx"
+        target="_blank"
+        rel="noopener noreferrer"
+        className="social-icon-link"
+      >
+        <Image src="/icon_discord.svg" alt="Discord" width={20} height={20} />
+      </a>
+      <a
+        href="https://twitter.com/phanft_official"
+        target="_blank"
+        rel="noopener noreferrer"
+        className="social-icon-link"
+      >
+        <Image src="/icon_twitter.svg" alt="Twitter" width={20} height={20} />
+      </a>
+      <a
+        href="https://instagram.com/phanft_official"
+        target="_blank"
+        rel="noopener noreferrer"
+        className="social-icon-link"
+      >
+        <Image
+          src="/icon_instagram.svg"
+          alt="Instagram"
+          width={20}
+          height={20}
+        />
+      </a>
+    </Box>
+    <Typography variant="overline" component="p">
       &copy; 2022 | Property of{" "}
       <Link
         href="https://quantegylabs.com"
