0ebf9f41dc743058a10c6c1e0b7555be127a08fe
Drew Cook <drewcook29@gmail.com>
2022-08-31 18:55:39 -0600

    chore: add new pages

---
+++ pages/roadmap.tsx
@@ -0,0 +1,22 @@
+import { Box, Container, Grid, Typography } from '@mui/material'
+import type { NextPage } from 'next'
+import Head from 'next/head'
+import AppLayout from '../components/layouts/AppLayout'
+import SectionRoadmap from '../components/sections/SectionRoadmap'
+
+const RoadmapPage: NextPage = () => {
+	return (
+		<>
+			<Head>
+				<title>PhanFT | Let's Get This Show On The Roadmap to Web3</title>
+			</Head>
+			<AppLayout>
+				<Box sx={{ my: 8 }}>
+					<SectionRoadmap />
+				</Box>
+			</AppLayout>
+		</>
+	)
+}
+
+export default RoadmapPage
